99d811402a9d77173f24e59207e8786986c25c42
speculadora <spencerharrison0@gmail.com>
2020-03-06 21:52:46 -0500

    Small changes to html, added code to handle table filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,18 +1,47 @@
 // from data.js
 var tableData = data;
 
-// use d3 to select the table body
+// select the table body
 var tbody = d3.select("tbody");
 
-// use forEach to iterate thru data
-tableData.forEach((ufoSighting) => {
-    // append a new row to the table body
-    var row = tbody.append("tr");
-    
-    // use forEach to store each paired value in the sighting to a cell
-    Object.entries(ufoSighting).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
-        console.log(value)
+// define a function to render table
+function renderTable(dataset) {
+    // use forEach to iterate thru data
+    dataset.forEach((item) => {
+        // append a new row to the table body
+        var row = tbody.append("tr");
+        
+        // use forEach to store each paired value in the sighting to a cell
+        Object.entries(item).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
+};
+
+renderTable(tableData);
+
+// select the 'filter table' button
+var filterSubmit = d3.select("#filter-btn");
+
+// function to handle what happens on a click
+filterSubmit.on("click", function() {
+    // select the text from the filter form
+    var dateToFilter = d3.select("#datetime").property("value");
+
+    // re-render the original table if the filter date is empty
+    if (dateToFilter == '') {
+        renderTable(tableData);
+        return;
+    }
+    else {
+        // otherwise clear the html inside the tbody
+        tbody.html('');
+    };
+
+    // create a new dataset based using a filtered version of the original
+    var filteredData = tableData.filter(sighting => sighting.datetime === dateToFilter);
+
+    // re-run the renderTable function using the filtered data
+    renderTable(filteredData);
 });
