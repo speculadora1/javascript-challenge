6a72a998924bf2fb17dffe2d8921f7c8ee6b4ccd
speculadora <spencerharrison0@gmail.com>
2020-03-06 21:27:42 -0500

    Added code to append table data to empty tbody

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,18 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// use d3 to select the table body
+var tbody = d3.select("tbody");
+
+// use forEach to iterate thru data
+tableData.forEach((ufoSighting) => {
+    // append a new row to the table body
+    var row = tbody.append("tr");
+    
+    // use forEach to store each paired value in the sighting to a cell
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+        console.log(value)
+    });
+});
